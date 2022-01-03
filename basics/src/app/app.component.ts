b9327f058f1bdaaff3a3d49cc1ab7d6777b446d5
chourabi-dev <chourabitahers62@gmail.com>
2022-01-03 20:42:10 +0100

    latest

--- basics/src/app/app.component.ts
+++ basics/src/app/app.component.ts
@@ -18,8 +18,69 @@ export class AppComponent {
     new Employee("chourabi taher",'tchourabi@gmail.com','93863732',2008),
     new Employee("chourabi taher",'tchourabi@gmail.com','93863732',2010),
     
-    
-    
   ]
+  /*------------------------------------------------------------------------------------------ */
+/*
+  content='welcome to our app !! <strong>html test</strong>';
+  color='red';
+
+  clicked:boolean = false;
+
+
+  btnClick(){
+    this.clicked = ! this.clicked;
+  }*/
+
+  /**------------------ gallery ----------------- */
+/*
+  gallery:string[]=[
+    'https://lh3.googleusercontent.com/bXB6ueK2wyb44f8A5Vxgf0_JmmTThXr7cqhUY9vr133RZkGguV2WGKV-Q4LTimmijCgO2zD3p3FxpfXcT3MALfLP3UQo8q2VpvzRLkj0Gg=s626',
+    'https://img.wallpapersafari.com/desktop/1920/1080/30/70/hMkVKQ.jpg',
+    'https://www.luxewalls.com.au/wp-content/uploads/2017/01/588041c54f31e.jpg'
+  ]
+
+  selectedIndex = 0;
+
+  imgURL=this.gallery[this.selectedIndex];
+
+
+  next(){
+    this.selectedIndex++;
+
+    // 0 1 2 => 3
+    //          3
+    if (this.selectedIndex === this.gallery.length) {
+      this.selectedIndex = 0;
+    }
+    this.imgURL= this.gallery[this.selectedIndex];
+  }
+
+  
+  previous(){
+    this.selectedIndex--;
+
+    if (this.selectedIndex < 0) {
+      this.selectedIndex = (this.gallery.length -1);
+    }
+    
+    this.imgURL= this.gallery[this.selectedIndex];
+  }*/
+
+
+  /********************************************************** */
+
+  public x='0px';
+  public y='0px';
+
+
+  mouseIsMoving(event){
+    const x = event.clientX;
+    const y = event.clientY;
+
+    console.log(x,y);
+
+    this.x = x+'px';
+    this.y= y+'px';
+  }
 
 }
